3d1d5f70e0928193a6f1ffd87b7cdcccb0c13288
TriangleYJ <dpwns99@gmail.com>
2020-02-02 22:08:36 +0900

    socket library test 1

---
+++ socket_test.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.set('view engine', 'pug');
+app.set('views', 'projects/socket_test');
+
+let data_by_time = 0;
+
+let looper = setInterval(() => {
+    data_by_time++;
+}, 1000);
+
+app.get('/', (req, res) => {
+    res.render('client');
+});
+
+server.listen(80, () => {
+    console.log('server running on 80 port');
+});
+
+io.on('connection', socket => {
+
+    socket.on('conn', data => {
+        console.log('Client connected : ' + socket.id);
+    });
+
+    socket.on('forcedisconnect', () => {
+        socket.disconnect();
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected : ' + socket.id);
+    });
+
+});
+
+io.clients((err, clients) => {
+    if(err) throw err;
+    console.log(clients);
+});
